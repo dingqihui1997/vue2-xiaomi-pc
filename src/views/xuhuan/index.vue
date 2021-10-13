1984b813617e8380c17ab898fa85820673669f71
xuhuangaga <503131952@qq.com>
2021-10-13 12:40:30 +0800

    第一次测试

---
+++ src/views/xuhuan/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>胥环测试</div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props:{},
+  data () {
+    return {
+    }
+  },
+  components: {},
+  methods: {},
+  mounted() {},
+  computed: {},
+  watch: {}
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
